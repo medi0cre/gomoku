5d41353b9cb1add722c3164101999af67f28cff4
medi0cre <anirbanmistry5000@gmail.com>
2025-02-26 14:42:06 -0330

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 const WebSocket = require("ws");
-
+const players = [];
 const wss = new WebSocket.Server({ port : 8082 });
 
 wss.on("connection", ws => {
     console.log("New Client Connected");
+    players.push(ws);
 
+    
     ws.on("message", (event) => {
-        console.log(event.toString());
+        for(let i = 0 ; i < players.length ; i++) {
+            if(players[i] != ws)
+                players[i].send(event.toString());
+        }
     });
 
     ws.on("close", () => {
         console.log("Client has disconnected");
-
         
     });
 });
